d7cd1789786e0e7a5741ba388a37f29f05d7065b
gilpgcl <54386892+gilpgcl@users.noreply.github.com>
2020-09-21 10:42:57 -0500

    Update get.js

--- functions/get.js
+++ functions/get.js
@@ -1,18 +1,7 @@
-import fetch from 'node-fetch';
+const axios = require('axios')
 exports.handler = async event => {
   const URL = "https://firestore.googleapis.com/v1/projects/gilpgmp/databases/(default)/documents/mascota";
-  let respuesta = "";
-  try {
-    const resp = await fetch(URL);
-    if (resp.ok) {
-      respuesta = await resp.text();
-    } else {
-      respuesta = resp.statusText;
-    }
-  } catch (e) {
-    console.log(e);
-    respuesta = e.message;
-  }
+  const res = await axios.get(URL);
   return {
     statusCode: 200,
     body: respuesta
