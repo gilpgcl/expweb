d7133e6f88f8ffb92f081e8d263499617a31e2b1
gilpgcl <54386892+gilpgcl@users.noreply.github.com>
2020-09-21 10:54:48 -0500

    Update get.js

--- functions/get.js
+++ functions/get.js
@@ -1,4 +1,4 @@
-const fetch = request('node-fetch').default;
+const fetch = require('node-fetch').default;
 exports.handler = async event => {
   const URL_LUZ = "https://firestore.googleapis.com/v1/projects/gilpgmp/databases/(default)/documents/mascota";
   let respuesta = "";
