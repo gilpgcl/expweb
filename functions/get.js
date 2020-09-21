2110030879b52e253c48dd7bd76fb1f9ea4a4754
gilpgcl <54386892+gilpgcl@users.noreply.github.com>
2020-09-21 10:25:44 -0500

    Create get.js

---
+++ functions/get.js
@@ -0,0 +1,16 @@
+export.handler = async event => {
+  const URL_LUZ = `https://firestore.googleapis.com/v1/projects/gilpgmp/databases/(default)/documents/mascota;
+  let respuesta = "";
+  try {
+    const resp = await fetch(URL_LUZ);
+    if (resp.ok) {
+      respuesta = await resp.text();
+    } else {
+      respuesta = resp.statusText;
+    }
+  } catch (e) {
+    console.log(e);
+    respuesta = e.message;
+  }
+  return respuesta;
+}
