4855e215c881b65f81905d218af9dbe1e09680c2
gilpgcl <54386892+gilpgcl@users.noreply.github.com>
2020-09-21 09:44:52 -0500

    Update saludo.js

--- functions/saludo.js
+++ functions/saludo.js
@@ -1,8 +1,7 @@
-exports.handler = function(event, context, callback) {
-  // const nombre = event.queryStringParameters.nombre;
-  callback(null, {
+exports.handler = async event => {
+  const nombre = event.queryStringParameters.nombre;
+  return {
     statusCode: 200,
-    body: "Saludos"
-//    body: `Mola ${nombre}`
+    body: `Hola ${nombre}`
   });
 }
