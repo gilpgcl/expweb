ff8e294a79f78ef9508e2014059a633840fae6fc
gilpgcl <54386892+gilpgcl@users.noreply.github.com>
2020-09-21 10:29:08 -0500

    Update get.js

--- functions/get.js
+++ functions/get.js
@@ -1,5 +1,5 @@
 exports.handler = async event => {
-  const URL_LUZ = `https://firestore.googleapis.com/v1/projects/gilpgmp/databases/(default)/documents/mascota;
+  const URL_LUZ = "https://firestore.googleapis.com/v1/projects/gilpgmp/databases/(default)/documents/mascota";
   let respuesta = "";
   try {
     const resp = await fetch(URL_LUZ);
